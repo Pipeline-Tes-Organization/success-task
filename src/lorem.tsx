2d35006606d972aebabd1e7cf259db04a03c6fe9
Dimas Maulana Ahmad <97804178+dimsmaul@users.noreply.github.com>
2026-02-27 10:36:44 +0700

    feat: add Lorem component to display placeholder text

---
+++ src/lorem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lorem = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, repellat harum, odio veniam dolore cupiditate sapiente, officiis modi repellendus incidunt unde? Animi non suscipit rerum culpa! Amet alias unde tempore.
+    </div>
+  )
+}
+
+export default Lorem
