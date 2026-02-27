f2874bdc49a7e4d716a52da07717c0348ca08aa1
Dimas Maulana Ahmad <97804178+dimsmaul@users.noreply.github.com>
2026-02-27 13:45:13 +0700

    [DMA-010] feat: refine wording in App component header and button text

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Ini Vite + React Testing Success Application</h1>
+      <h1>Ini Vite + React Testing Success</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 100)}>
-          count is {count}
+          count is {count} Updated
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
