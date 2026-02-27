371373937ab8d660291e1f9a21c643e83e903ea3
Dimas Maulana Ahmad <97804178+dimsmaul@users.noreply.github.com>
2026-02-27 13:46:37 +0700

    feat: update button text to include 'again' for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Ini Vite + React Testing Success</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 100)}>
-          count is {count} Updated
+          count is {count} Updated again
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
